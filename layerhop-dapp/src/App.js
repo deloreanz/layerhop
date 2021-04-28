8bb60fa64c9afaab8fd46c6a8a487148ff573207
Bryan Carter <deloreanz@gmail.com>
2021-04-28 16:25:16 -0700

    trying to get hot reloading working

--- layerhop-dapp/src/App.js
+++ layerhop-dapp/src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Grid,
+  Typography,
+  Paper,
+} from '@material-ui/core';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 
 // Update with the contract address logged out to the CLI when it was deployed 
 const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default () => {
   // store greeting in local state
+  const classes = useStyles();
   const [greeting, setGreetingValue] = useState();
 
   // request access to the user's MetaMask account
@@ -44,15 +66,18 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>{greeting}</div>
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
-      </header>
-    </div>
+    <Grid container className={classes.root} spacing={2} sm={12}>
+      <Grid container item justify='center' sm={12}>
+        <Paper className={classes.paper}>
+          {/* <Typography>{greeting}</Typography> */}
+          <Button
+            type='primary'
+            onClick={fetchGreeting}>Fetch Greeting!</Button>
+          <Button
+            onClick={setGreeting}>Set Greeting</Button>
+          <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        </Paper>
+      </Grid>
+    </Grid>
   );
-}
-
-export default App;
+};
